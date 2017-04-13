7a078f341e51f1ce54d9e96115db98ee4e5944f6
dani maietta <dani maietta>
2017-04-12 16:56:26 -1200

    Dani Changes, Delete ConnectionDB, Add RiskManagement Backend, click change list

--- Planning/WebContent/js/riskManagement.js
+++ Planning/WebContent/js/riskManagement.js
@@ -5,33 +5,84 @@ angular.module('myApp.riskManagement', [])
 .controller('riskManagementViewCtrl', ['$scope','$state',function($scope,$state) {
 	
 	$scope.risks = [
-					{description: "computer goes down", probability: 10, impact: 4},
-					{description: "computer get virus", probability: 20, impact: 3},
-					{description: "computer run out of RAM", probability: 50, impact: 1},
-				   ];
-	 
-	 $(function(){
-		    $('.editText').dblclick( function(){
-		    	console.log("1> "+$scope.risks[a].description);
-		        var text = $scope.risks[a].description;
-		        $(this).html('<input type="text" value="'+text+'">').find('input').focus();
-		    }).keypress( function(e){
-		        if(e.keyCode == 13){
-		        	$scope.risks[a].description = $('input', this).val();
-		        	text = $scope.risks[a].description;
-		            $(this).html( text );
-		            console.log("2> "+$scope.risks[a].description);
-		        }
-		    });
-	 });
-	 
-	 
-	 $scope.test = function(){
-		 console.log($scope.risks[1].description);
-	 }
+		{description: "computer goes down", probability: 10, impact: 4},
+		{description: "computer get virus", probability: 20, impact: 3},
+		{description: "computer run out of RAM", probability: 50, impact: 1},
+	   ];
+
+	//Background config
+	
+	var imgs = ['images/office1.jpg',
+	    'images/office2.jpg',
+	    'images/office3.jpg',
+	    'images/office4.jpg',
+	    'images/office5.jpg'
+	    ];
+	
+	var i = 1; 
+	document.getElementById("bgimg").style.backgroundImage = "url("+imgs[i]+")";
+	setTimeout(changeImage, 10000);
+	
+	function changeImage(){
+		i++;
+		if(i === 4){
+			i = 0;
+		}
+		$("#bgimg").fadeOut(200, function(){
+			document.getElementById("bgimg").style.backgroundImage = "url("+imgs[i]+")";
+			$("#bgimg").fadeIn(200);
+		});
+		setTimeout(changeImage, 10000);
+	}
+	
+	// Edit Description
+	
+	var dindex; // description index
+	$scope.editDescription = function(index){
+		dindex = index;
+	}
+	
+	$(function(){
+	$('.editDescription').dblclick( function(){
+		console.log("1> "+$scope.risks[dindex].description);
+	    var text = $scope.risks[dindex].description;
+	    $(this).html('<input type="text" value="'+text+'">').find('input').focus();
+	}).keypress( function(e){
+	    if(e.keyCode == 13){
+	    	$scope.risks[dindex].description = $('input', this).val();
+	    	text = $scope.risks[dindex].description;
+	        $(this).html( text );
+	        console.log("2> "+$scope.risks[dindex].description);
+	    }
+	});
+	});
+	
+	// Edit Probability
+	
+	var pindex; // probability index
+	$scope.editProbability = function(index){
+		pindex = index;
+		console.log(pindex);
+	}
+	
+	$(function(){
+	    $('.editProbability').dblclick( function(){
+	    	var value = $scope.risks[pindex].probability;
+	    	console.log("1> "+$scope.risks[pindex].probability);
+	        $(this).html('<input type="number" value="'+value+'">').find('input').focus();
+	    }).keypress( function(e){
+	        if(e.keyCode == 13){
+	        	$scope.risks[pindex].probability = $('input', this).val();
+	        	value = $scope.risks[pindex].probability;
+	            $(this).html( value );
+	            console.log("2> "+$scope.risks[pindex].probability);
+	        }
+	    });
+	});
+	
+	
+	$scope.test = function(){
+		RiskManagementDB.addRiskManagement();
+	}
  
 }]);
-
-function test(){
-	RiskManagementDB.printFutbolistas();
-}
