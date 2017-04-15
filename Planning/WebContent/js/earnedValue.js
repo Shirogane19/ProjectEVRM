47d9ac98b1fc70b06b6a5e1f4db64c8a8f78b1ad
Randall Liu Lin <katana20@hotmail.com>
2017-04-14 20:05:20 -0600

    Earned Value, Back end - Completo Front end - Llamados al server funcionando                    - CRUD completo                    - Calculos del Earned value completo                    - Funciones del HTML, Javascript completo Solo falta recibir el nombre del proyecto

--- Planning/WebContent/js/earnedValue.js
+++ Planning/WebContent/js/earnedValue.js
@@ -2,8 +2,172 @@
 
 angular.module('myApp.earnedValue', [])
 
-.controller('earnedValueViewCtrl', ['$scope','$state',function($scope,$state) {
+.controller('earnedValueViewCtrl', ['$scope','$state','$timeout', '$sce', function($scope,$state,$timeout,$sce) {
 
-	console.log("Valor Ganado");
+	//console.log("Valor Ganado");
 
-}]);
+	$scope.evs = []; // Coleccion de valores ganados
+	$scope.inputPV; //Valor planeado
+	$scope.faseN; //cantidad de fases en la gestion de valor ganado
+
+	///-------------------------------------------------------------
+	$scope.insertEV = function(){
+
+		//console.log("Insertar");
+
+		var evObject = { fase: $scope.faseN, pv: $scope.inputPV, av: "", porcentajeEV: "", nonbreProyecto: "solo bueno" };
+
+		EarnedValueDB.insert(evObject, function(data){
+			//console.log(data);
+			$scope.getAllEarnedValue();
+			$scope.inputPV = "";
+		});
+
+	}///
+	///-------------------------------------------------------------
+	$scope.getAllEarnedValue = function(){
+
+		//console.log("Get All EV");
+
+		var requestObject = { nombreProyecto: "solo bueno" };
+
+		EarnedValueDB.getCollection(requestObject, function(data){
+			//console.log(data);
+			$scope.$apply(function(){
+				$scope.evs = data;
+				$scope.faseN = $scope.evs.length + 1;
+			});
+		});
+
+	}///
+	///-------------------------------------------------------------
+	$scope.updateEV = function(e){
+
+		//console.log("UpdateEV");
+
+		e.av = $scope.validateValue(e.av);
+		e.pv = $scope.validateValue(e.pv);
+		e.porcentajeEV = $scope.validateValue(e.porcentajeEV);
+
+		var objectToUpdate = { fase: e.fase, pv: e.pv, av: e.av, porcentajeEV: e.porcentajeEV, nonbreProyecto: e.nonbreProyecto };
+
+		EarnedValueDB.save(objectToUpdate, function(data){
+			//console.log(data);
+			$state.reload();
+		});
+
+	}///
+	///-------------------------------------------------------------
+	$scope.calcularEV = function (PV, EV) { 
+		if (isNaN(parseFloat(PV) * parseFloat(EV) / 100)) {
+	    	return "---";
+	    }
+	    return (parseFloat(PV) * parseFloat(EV) / 100).toFixed(2);
+    }
+	///-------------------------------------------------------------
+	$scope.calcularCV = function (PV,EV,AV) {
+		if (isNaN(parseFloat(PV) * parseFloat(EV) / 100)) {
+		    return "---";
+		}else{
+			var ev =  parseFloat(PV) * parseFloat(EV) / 100;
+			if(isNaN(ev - parseFloat(AV))){
+				return "---";
+			}else{
+				return (ev - parseFloat(AV)).toFixed(2);
+			}
+		}
+    }
+	///-------------------------------------------------------------
+	$scope.calcularSV = function (PV,EV) {
+		if (isNaN(parseFloat(PV) * parseFloat(EV) / 100)) {
+		    return "---";
+		}else{
+			var ev =  parseFloat(PV) * parseFloat(EV) / 100;
+			return (ev - parseFloat(PV)).toFixed(2);
+		}
+    }
+    ///-------------------------------------------------------------
+	$scope.calcularCPI = function (PV,EV,AV) {
+		if (isNaN(parseFloat(PV) * parseFloat(EV) / 100)) {
+		    return "---";
+		}else{
+			var ev =  parseFloat(PV) * parseFloat(EV) / 100;
+			if(isNaN(ev / parseFloat(AV))){
+				return "---";
+			}else{
+				return (ev / parseFloat(AV)).toFixed(2);
+			}
+		}
+    }
+    ///-------------------------------------------------------------
+	$scope.calcularSPI = function (PV,EV) {
+		if (isNaN(parseFloat(PV) * parseFloat(EV) / 100)) {
+		    return "---";
+		}else{
+			var ev =  parseFloat(PV) * parseFloat(EV) / 100;
+			if(isNaN(ev / parseFloat(PV))){
+				return "---";
+			}else{
+				return (ev / parseFloat(PV)).toFixed(2);
+			}
+		}
+    }
+    ///-------------------------------------------------------------
+    $scope.generateColor = function (value) {
+    	if(value > 0){
+    		return "green";
+    	}else{
+    		if(value == 0){
+    			return "blue";
+    		}else{
+    			return "red";
+    		}
+    	}
+    }
+    ///-------------------------------------------------------------
+    $scope.generateColor2 = function (value) {
+    	if(value > 1){
+    		return "green";
+    	}else{
+    		if(value == 1){
+    			return "blue";
+    		}else{
+    			return "red";
+    		}
+    	}
+    }
+    ///-------------------------------------------------------------
+    $scope.validateValue = function(value){
+    	if(value.length === 0 || value === "null" || isNaN(parseFloat(value))){
+			return 0;
+		}else{
+			if(value.length === parseFloat(value).toString().length){
+				return parseFloat(value);
+			}else{
+				return 0;
+			}
+		}
+    }
+    ///-------------------------------------------------------------
+    $scope.reload = function(){
+    	$state.reload();
+    }
+    ///-------------------------------------------------------------
+    $scope.getAllEarnedValue();
+
+}])////controlador
+
+.directive('stringToNumber', function() {
+  	return {
+	    require: 'ngModel',
+	    link: function(scope, element, attrs, ngModel) {
+	        ngModel.$parsers.push(function(value) {
+	        	return '' + value;
+	      	});
+	      	ngModel.$formatters.push(function(value) {
+	        	return parseFloat(value);
+	    });
+    }
+};////directiva
+  
+});//
--- Planning/src/com/java/mongo/EarnedValue.java
+++ Planning/src/com/java/mongo/EarnedValue.java
@@ -1,5 +1,104 @@
 package com.java.mongo;
 
+import java.util.ArrayList;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+
 public class EarnedValue {
+	
+	private String id;
+	private String fase;
+	private String pv;
+	private String av;
+	private String porcentajeEV;
+	private String nonbreProyecto;
+	
+	public EarnedValue(String id, String fase, String pv, String av, String porcentajeEV, String nonbreProyecto) {
+		this.id = id;
+		this.fase = fase;
+		this.pv = pv;
+		this.av = av;
+		this.porcentajeEV = porcentajeEV;
+		this.nonbreProyecto = nonbreProyecto;
+	}
+	
+	public EarnedValue(String fase, String pv, String av, String porcentajeEV, String nonbreProyecto) {
+		this.fase = fase;
+		this.pv = pv;
+		this.av = av;
+		this.porcentajeEV = porcentajeEV;
+		this.nonbreProyecto = nonbreProyecto;
+	}
+	
+	
+	// Transformo un objecto que me da MongoDB a un Objecto Java
+	public EarnedValue(BasicDBObject dBObjectEV) {
+		this.id = dBObjectEV.getString("id");
+		this.fase = dBObjectEV.getString("fase");
+		this.pv = dBObjectEV.getString("pv");
+		this.av = dBObjectEV.getString("av");
+		this.porcentajeEV = dBObjectEV.getString("porcentajeEV");
+		this.nonbreProyecto = dBObjectEV.getString("nonbreProyecto");
+	}
+
+	public BasicDBObject toDBObjectEarnedValue() {
+		// Creamos una instancia BasicDBObject
+		BasicDBObject dBObjectEV = new BasicDBObject();
+		
+		if(this.getId()!=null){
+			dBObjectEV.append("id", this.getId());
+		}
+		dBObjectEV.append("fase", this.getFase());
+		dBObjectEV.append("pv", this.getPv());
+		dBObjectEV.append("av", this.getAv());
+		dBObjectEV.append("porcentajeEV", this.getPorcentajeEV());
+		dBObjectEV.append("nonbreProyecto", this.getNonbreProyecto());
+
+		return dBObjectEV;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFase() {
+		return fase;
+	}
+	public void setFase(String fase) {
+		this.fase = fase;
+	}
+	public String getPv() {
+		return pv;
+	}
+	public void setPv(String pv) {
+		this.pv = pv;
+	}
+	public String getAv() {
+		return av;
+	}
+	public void setAv(String av) {
+		this.av = av;
+	}
+	public String getPorcentajeEV() {
+		return porcentajeEV;
+	}
+	public void setPorcentajeEV(String porcentajeEV) {
+		this.porcentajeEV = porcentajeEV;
+	}
+	public String getNonbreProyecto() {
+		return nonbreProyecto;
+	}
+	public void setNonbreProyecto(String nonbreProyecto) {
+		this.nonbreProyecto = nonbreProyecto;
+	}
+	
+	
+	
+	
+	
 
 }
